d6626b7936276f3814e4518f18fb16bf584235c2
Rosita Purwaningsih <rositaprwnngsh@gmail.com>
2024-12-13 20:29:28 +0800

    adding functions on JS file

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const contacts = [
   {
-    id: 1,
+    id: 146,
     name: "Tamara",
     phone: "1234567890",
     email: "tamara@gmail.com",
   },
   {
-    id: 2,
+    id: 285,
     name: "Awan",
     phone: "0987654321",
     email: "awan@gmail.com",
   },
   {
-    id: 3,
+    id: 334,
     name: "Rosita",
     phone: "0987654321",
     email: "rosita@gmail.com",
@@ -26,4 +26,30 @@ function getContacts() {
   });
 }
 
+function generateId(params) {
+  return Math.floor(Math.random() * 1000);
+}
+
+function addContact() {
+  const newContact = {
+    id: generateId(),
+    name: "Saka",
+    phone: "0987654321",
+    email: "saka@gmail.com",
+  };
+
+  const result = contacts.push(newContact);
+}
+
+function searchContact(keyword) {
+  const searchContact = contacts.filter((contact) => {
+    return contact.name.toLowerCase().includes(keyword.toLowerCase());
+  });
+
+  console.log(searchContact);
+}
+
 getContacts();
+addContact();
+
+searchContact("Rosita");
